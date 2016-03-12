f19c564241f3bf6a281e8a47110e7ee40a024341
Arunan Sugunakumar <arunans.14@cse.mrt.ac.lk>
2016-03-12 23:57:40 +0530

    Initial project	

---
+++ SmartTailor/SmartTailor/KinectControl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+using KinectUtils;
+
+namespace SmartTailor;
+{
+    class KinectControl
+    {
+        KinectSensor sensor;
+
+        CoordinateMapper coordinateMapper = null;
+        MultiSourceFrameReader reader;
+        bool bStopAfterFrameInterpolation = false;
+
+        KinectUtils.BackgroundRemoval backgroundRemovalTool;
+
+        private delegate void DispatcherCallback();
+        private ImageSource ImageScreenshot;
+
+
+        private int iFrameInterpolation = 0;
+        private int numberOfCutPoints = 0;
+        private List<ushort[]> pixelsList;
+
+        public void KinectConnect() {
+            sensor = KinectSensor.GetDefault();
+
+            if (sensor != null) {
+                sensor.Open();
+
+                reader = sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Depth | FrameSourceTypes.BodyIndex);
+                //_reader = _sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Depth);
+                reader.MultiSourceFrameArrived += Reader_MultiSourceFrameArrived;
+                this.ImageScreenshot = new WriteableBitmap(this.sensor.DepthFrameSource.FrameDescription.Width, this._sensor.DepthFrameSource.FrameDescription.Height, 96.0, 96.0, PixelFormats.Gray8, null);
+                coordinateMapper = sensor.CoordinateMapper;
+                backgroundRemovalTool = new BackgroundRemoval(sensor.CoordinateMapper);
+            }
+        }
+    }
+}
---
+++ SmartTailor/SmartTailor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+
+namespace SmartTailor
+{
+    static class SmartTailor
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Console.WriteLine("Staring...");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            //Console.WriteLine("Hello World!!!");
+            //Console.ReadKey();
+
+            //System.Runtime nui = System.Runtime.Kinect[0];
+        }
+    }
+}
